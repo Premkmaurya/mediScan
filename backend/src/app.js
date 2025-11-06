9e9176b49766d1f6fb94ce674c5e3b54bb53f086
Premkmaurya <premkmaurya123456@gmail.com>
2025-11-06 15:58:48 +0530

    sixth commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express= require("express")
 const authRoutes = require("./routes/auth.routes")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
+const path = require('path')
 
 const app = express()
 
